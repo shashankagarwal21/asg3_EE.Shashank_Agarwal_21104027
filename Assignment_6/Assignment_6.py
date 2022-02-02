1ebfd2b7671a204916c8f563afc22732c0e3d22e
shashankagarwal21 <shashankagarwal.bt21ele@pec.edu.in>
2022-02-02 22:43:43 +0530

    rev 2

--- Assignment_6/Assignment_6.py
+++ Assignment_6/Assignment_6.py
@@ -1,14 +1,14 @@
-true_list = ["y", "Y", "yes", "Yes", "YES"]
-false_list = ["n", "N", "no", "No", "NO"]
-choice = input("Do you want to add a student (y/n) ")
+true_list = ["y", "Y", "yes", "Yes", "YES"]  # List to store yes conditions
+false_list = ["n", "N", "no", "No", "NO"]  # List to store no conditions
+choice = input("Do you want to add a student (y/n) ")  # take input from user
 temp_key = 0
-data = {}
-value_sorted_data = {}
-key_sorted_data = {}
+data = {}  # dictionary to store user entered data
+value_sorted_data = {}  # dictionary to store value sorted data
+key_sorted_data = {}  # dictionary to store key sorted data
 
 # Input Loop
 while True:
-
+    # if choice is yes take input, if no then exit list, else ask user for valid input
     if choice in true_list:
         temp_key = int(input("Enter student sid "))
         data[temp_key] = input("Enter student name ")
