c11655923b95927cd617cbd8467b393fd49d2199
shashankagarwal21 <shashankagarwal.bt21ele@pec.edu.in>
2022-02-05 19:21:57 +0530

    rev 6

--- Assignment_2/Assignment_2.py
+++ Assignment_2/Assignment_2.py
@@ -1,7 +1,8 @@
 month_upper = [1, 3, 5, 7, 8, 10, 12]  # list of month with 31 days
-date = input("Enter date in DD/MM/YYYY format ")  # input from user
+date = input("Enter date in DD/MM/YYYY format : ")  # input from user
 leap_check = False
 day_limit = 0  # limit of days in month
+
 
 # input loop
 # if input date is invalid then ask user to enter date again
@@ -11,42 +12,34 @@ while True:
     month = int(list_date[1])
     year = int(list_date[2])
 
-    if (1 <= day <= 31) & (1800 <= year <= 2025) & (1 <= month <= 12):
-        break
-
-    else:
-        date = input("Enter a valid date between year 1800 and 2025 ")
-
-# check if year is leap year or not
-if year % 100 == 0:
-    if year % 400 == 0:
+    # check if year is leap year or not
+    if year % 100 == 0:
+        if year % 400 == 0:
+            leap_check = True
+        else:
+            leap_check = False
+    elif year % 4 == 0:
         leap_check = True
     else:
         leap_check = False
-elif year % 4 == 0:
-    leap_check = True
-else:
-    leap_check = False
 
-# check what is number of days in month
-if month in month_upper:
-    day_limit = 31
-elif month == 2:
-    if leap_check:
-        day_limit = 29
+    # check what is number of days in month
+    if month in month_upper:
+        day_limit = 31
+    elif month == 2:
+        if leap_check:
+            day_limit = 29
+        else:
+            day_limit = 28
     else:
-        day_limit = 28
-else:
-    day_limit = 30
+        day_limit = 30
 
-# check if day entered lies in month or not
-# if not ask user to enter it again
-while day > day_limit:
-    date = input("Enter a valid date between year 1800 and 2025 ")
-    list_date = date.split("/")
-    day = int(list_date[0])
-    month = int(list_date[1])
-    year = int(list_date[2])
+    if (1 <= day <= day_limit) & (1800 <= year <= 2025) & (1 <= month <= 12):
+        break
+
+    else:
+        date = input("Enter a valid date : ")
+
 
 day = day + 1  # add 1 to the current day
 
