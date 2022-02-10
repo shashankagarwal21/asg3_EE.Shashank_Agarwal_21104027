ef8b982b2ee89cd5598cc8ee6460acfe1a9092e7
shashankagarwal21 <shashankagarwal.bt21ele@pec.edu.in>
2022-02-10 23:06:29 +0530

    rev 9

--- Assignment_7/Assignment_7.py
+++ Assignment_7/Assignment_7.py
@@ -1,25 +1,31 @@
 fib = [0, 1]  # First 2 terms of fibonacci sequence
 new_ele = 0  # variable to add next number
 average = 0  # variable for average
+sum_terms = 0  # variable for sum of terms
 
 num_terms = int(input("How may terms would you like to calculate "))  # Number of element in list
 
-# if num_term > 2 then add the sequence to the list fib and print it
+# If entered number is negative ask user to enter it again
+while num_terms <= 0:
+    num_terms = int(input("Enter a positive integer "))
+
+# if num_term > 2 then add the sequence to the list fib
 if num_terms > 2:
     for i in range(2, num_terms):
         new_ele = fib[i - 1] + fib[i - 2]
         fib.append(new_ele)
-    average = (sum(fib)/num_terms)
-    print("Sequence is " + str(fib))
-    print("Average is " + str(average))
 
-# else if num_term = 1 then 0 is sequence and if num_term = 2 then sequence is 0, 1
-else:
-    if num_terms == 1:
-        print("sequence is " + str(fib[0]))
-        print("Average is 0")
-    elif num_terms == 2:
-        print("sequence is " + str(fib[0]) + ", " + str(fib[1]))
-        print("Average is 0.5")
-    else:
-        print("error:Enter a positive integer")
+# Calculate sum of terms in sequence
+for i in range(num_terms):
+    sum_terms = sum_terms + fib[i]
+
+# calculate average of sequence
+average = (sum_terms/num_terms)
+
+# Print the sequence
+print("Sequence is ", end="")
+for i in range(num_terms):
+    print(fib[i], end=" ")
+
+# Print the average of sequence
+print("\nAverage is " + str(average))
