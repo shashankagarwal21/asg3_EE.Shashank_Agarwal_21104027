c0e7e45f19d83ba32295d7ecc6d7814d3c834d4c
shashankagarwal21 <shashankagarwal.bt21ele@pec.edu.in>
2022-02-12 23:16:47 +0530

    rev 10

--- Assignment_7/Assignment_7.py
+++ Assignment_7/Assignment_7.py
@@ -1,9 +1,8 @@
 fib = [0, 1]  # First 2 terms of fibonacci sequence
-new_ele = 0  # variable to add next number
 average = 0  # variable for average
-sum_terms = 0  # variable for sum of terms
 
-num_terms = int(input("How may terms would you like to calculate "))  # Number of element in list
+# Number of element in list
+num_terms = int(input("How may terms would you like to calculate "))
 
 # If entered number is negative ask user to enter it again
 while num_terms <= 0:
@@ -15,17 +14,12 @@ if num_terms > 2:
         new_ele = fib[i - 1] + fib[i - 2]
         fib.append(new_ele)
 
-# Calculate sum of terms in sequence
-for i in range(num_terms):
-    sum_terms = sum_terms + fib[i]
-
 # calculate average of sequence
-average = (sum_terms/num_terms)
+average = (sum(fib[0:num_terms])/num_terms)
 
 # Print the sequence
 print("Sequence is ", end="")
-for i in range(num_terms):
-    print(fib[i], end=" ")
+print(fib[0:num_terms])
 
 # Print the average of sequence
-print("\nAverage is " + str(average))
+print("Average is " + str(average))
